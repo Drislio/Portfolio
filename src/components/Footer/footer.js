cb4adc2880edad32df881d24d5ff1270e80be826
Drislio <moni9009@abv.bg>
2021-02-26 16:35:43 +0100

    adding footer

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -1,9 +1,20 @@
 import "../Footer/footer.css"
 
+import logo1 from "../../assets/GithubLogo.png"
+import logo2 from "../../assets/LinkedlnLogo.png"
+import logo3 from "../../assets/InstagramLogo.png"
+
 const Footer = () =>{
     return(
-        <div>
-            
+        <div className="footer">
+            <div className="container footerContent">
+                <span id="email">moni9009@abv.bg</span>
+                <div className="linebreaking">
+                        <a href=""><img className="logos footer" src={logo1}/></a>
+                        <a href=""><img className="logos linkedln footer" src={logo2}/></a>
+                        <a href=""><img className="logos footer" src={logo3}/></a>
+                </div>
+            </div>
         </div>
     )
 }
